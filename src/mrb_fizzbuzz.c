c4081ff672c890e9043cc4ef6af7e1a209371344
Masaya Konishi <konishi@office-ore.net>
2014-07-17 20:14:22 +0900

    FizzBuzzの結果を返すようになった
    Arrayの中身が整数以外の場合についてのチェック入れていない

--- src/mrb_fizzbuzz.c
+++ src/mrb_fizzbuzz.c
@@ -7,62 +7,42 @@
 */
 
 #include "mruby.h"
+#include "mruby/value.h"
+#include "mruby/array.h"
+#include "mruby/range.h"
+#include "mruby/hash.h"
 #include "mruby/data.h"
 #include "mrb_fizzbuzz.h"
 
-#define DONE mrb_gc_arena_restore(mrb, 0);
-
-typedef struct {
-  char *str;
-  int len;
-} mrb_fizzbuzz_data;
-
-static const struct mrb_data_type mrb_fizzbuzz_data_type = {
-  "mrb_fizzbuzz_data", mrb_free,
-};
-
-static mrb_value mrb_fizzbuzz_init(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_fizzbuzz(mrb_state *mrb, mrb_value self)
 {
-  mrb_fizzbuzz_data *data;
-  char *str;
-  int len;
+  mrb_int i;
+  mrb_value v, value, fizzbuzz;
 
-  data = (mrb_fizzbuzz_data *)DATA_PTR(self);
-  if (data) {
-    mrb_free(mrb, data);
+  value = mrb_ary_new_capa(mrb, RARRAY_LEN(self));
+  for (i = 0; i < RARRAY_LEN(self); ++i) {
+    v = RARRAY_PTR(self)[i];
+    
+    if((v.value.i % 15) == 0){
+      fizzbuzz = mrb_str_new_cstr(mrb, "FizzBuzz");
+    } else if((v.value.i % 5) == 0) {
+      fizzbuzz = mrb_str_new_cstr(mrb, "Buzz");
+    } else if((v.value.i % 3) == 0) {
+      fizzbuzz = mrb_str_new_cstr(mrb, "Fizz");
+    } else {
+      fizzbuzz = v;
+    }
+    mrb_ary_set(mrb, value, i, fizzbuzz);
   }
-  DATA_TYPE(self) = &mrb_fizzbuzz_data_type;
-  DATA_PTR(self) = NULL;
 
-  mrb_get_args(mrb, "s", &str, &len);
-  data = (mrb_fizzbuzz_data *)mrb_malloc(mrb, sizeof(mrb_fizzbuzz_data));
-  data->str = str;
-  data->len = len;
-  DATA_PTR(self) = data;
-
-  return self;
-}
-
-static mrb_value mrb_fizzbuzz_hello(mrb_state *mrb, mrb_value self)
-{
-  mrb_fizzbuzz_data *data = DATA_PTR(self);
-
-  return mrb_str_new(mrb, data->str, data->len);
-}
-
-static mrb_value mrb_fizzbuzz_hi(mrb_state *mrb, mrb_value self)
-{
-  return mrb_str_new_cstr(mrb, "hi!!");
+  return value;
 }
 
 void mrb_mruby_fizzbuzz_gem_init(mrb_state *mrb)
 {
-    struct RClass *fizzbuzz;
-    fizzbuzz = mrb_define_class(mrb, "FizzBuzz", mrb->object_class);
-    mrb_define_method(mrb, fizzbuzz, "initialize", mrb_fizzbuzz_init, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, fizzbuzz, "hello", mrb_fizzbuzz_hello, MRB_ARGS_NONE());
-    mrb_define_class_method(mrb, fizzbuzz, "hi", mrb_fizzbuzz_hi, MRB_ARGS_NONE());
-    DONE;
+    struct RClass *a = mrb->array_class;
+
+    mrb_define_method(mrb, a, "fizzbuzz", mrb_fizzbuzz, MRB_ARGS_REQ(1));
 }
 
 void mrb_mruby_fizzbuzz_gem_final(mrb_state *mrb)
