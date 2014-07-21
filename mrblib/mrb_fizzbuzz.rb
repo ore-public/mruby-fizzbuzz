aada452a7b24d7d064cd14a904b1d8a3ab7bba54
Masaya Konishi <konishi@office-ore.net>
2014-07-21 10:19:51 +0900

    Ruby実装のfizzbuzz2メソッドを追加

--- mrblib/mrb_fizzbuzz.rb
+++ mrblib/mrb_fizzbuzz.rb
@@ -1,5 +1,15 @@
 class Array
   def fizzbuzz2
-    
+    self.map do |v|
+      if (v % 15) == 0
+        'FizzBuzz'
+      elsif (v % 3) == 0
+        'Fizz'
+      elsif (v % 5) == 0
+        'Buzz'
+      else
+        v
+      end
+    end    
   end
 end
--- test/mrb_fizzbuzz.rb
+++ test/mrb_fizzbuzz.rb
@@ -9,3 +9,10 @@ assert("Array#fizzbuzz") do
     array.fizzbuzz)
 end
 
+assert("Array#fizzbuzz2") do
+  array = (1..16).to_a
+  assert_equal(
+    [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz',16],
+    array.fizzbuzz2)
+end
+
